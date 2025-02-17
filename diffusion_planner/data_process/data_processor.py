12b7f772ad9afd62c7eac4826072e0f73260db7e
cailiu2 <cailiu2@iflytek.com>
2025-02-17 08:54:19 +0000

    add main func of data processor

--- diffusion_planner/data_process/data_processor.py
+++ diffusion_planner/data_process/data_processor.py
@@ -2,30 +2,40 @@ import numpy as np
 
 from nuplan.common.actor_state.vehicle_parameters import get_pacifica_parameters
 from nuplan.common.actor_state.state_representation import Point2D
+from nuplan.planning.scenario_builder.nuplan_db.nuplan_scenario_utils import ScenarioMapping
+from nuplan.planning.scenario_builder.nuplan_db.nuplan_scenario_builder import NuPlanScenarioBuilder
+from nuplan.planning.scenario_builder.scenario_filter import ScenarioFilter
+from nuplan.planning.utils.multithreading.worker_parallel import SingleMachineParallelExecutor
+from nuplan.planning.utils.multithreading.worker_pool import Task
 
 from diffusion_planner.data_process.roadblock_utils import route_roadblock_correction
 from diffusion_planner.data_process.agent_process import (
-agent_past_process, 
+agent_past_process,
 sampled_tracked_objects_to_array_list,
 sampled_static_objects_to_array_list
 )
 from diffusion_planner.data_process.map_process import get_neighbor_vector_set_map, map_process
-from diffusion_planner.data_process.utils import convert_to_model_inputs
-
+from diffusion_planner.data_process.utils import (convert_to_model_inputs,
+get_scenario_map,
+get_filter_parameters
+)
 
 class DataProcessor(object):
-    def __init__(self, config):
+    def __init__(self, config, scenarios):
 
-        self.num_agents = config.agent_num
-        self.num_static = config.static_objects_num
+        self.num_agents = 20 #[int]
+        self.num_static = 20 #[int]
         self.max_ped_bike = 10 # Limit the number of pedestrians and bicycles in the agent.
-        self._radius = 100 # [m] query radius scope relative to the current pose.
+        self._radius = 80 # [m] query radius scope relative to the current pose.
 
         self._map_features = ['LANE', 'LEFT_BOUNDARY', 'RIGHT_BOUNDARY', 'ROUTE_LANES'] # name of map features to be extracted.
-        self._max_elements = {'LANE': config.lane_num, 'LEFT_BOUNDARY': config.lane_num, 'RIGHT_BOUNDARY': config.lane_num, 'ROUTE_LANES': config.route_num} # maximum number of elements to extract per feature layer.
-        self._max_points = {'LANE': config.lane_len, 'LEFT_BOUNDARY': config.lane_len, 'RIGHT_BOUNDARY': config.lane_len, 'ROUTE_LANES': config.route_len} # maximum number of points per feature to extract per feature layer.
+        self._max_elements = {'LANE': 40, 'LEFT_BOUNDARY': 10, 'RIGHT_BOUNDARY': 10, 'ROUTE_LANES': 10} # maximum number of elements to extract per feature layer.
+        self._max_points = {'LANE': 50, 'LEFT_BOUNDARY': 50, 'RIGHT_BOUNDARY': 50, 'ROUTE_LANES': 50} # maximum number of points per feature to extract per feature layer.
         self._vehicle_parameters = get_pacifica_parameters()
 
+        self._scenarios = scenarios
+        self.past_time_horizon = 2 #[seconds]
+        self._interpolation_method = 'linear' # Interpolation method to apply when interpolating to maintain fixed size map elements.
 
     def observation_adapter(self, history_buffer, traffic_light_data, map_api, route_roadblock_ids, device='cpu'):
 
@@ -56,10 +66,10 @@ class DataProcessor(object):
         coords, traffic_light_data, speed_limit, lane_route = get_neighbor_vector_set_map(
             map_api, self._map_features, ego_coords, self._radius, traffic_light_data
         )
-        vector_map = map_process(route_roadblock_ids, anchor_ego_state, coords, traffic_light_data, speed_limit, lane_route, self._map_features, 
+        vector_map = map_process(route_roadblock_ids, anchor_ego_state, coords, traffic_light_data, speed_limit, lane_route, self._map_features,
                                     self._max_elements, self._max_points)
 
-        
+
         data = {"neighbor_agents_past": neighbor_agents_past[:, -21:],
                 "ego_current_state": np.array([0., 0., 1. ,0.], dtype=np.float32), # ego centric x, y, cos, sin
                 "static_objects": static_objects}
@@ -67,3 +77,21 @@ class DataProcessor(object):
         data = convert_to_model_inputs(data, device)
 
         return data
+
+if __name__ == "__main__":
+  map_version = "nuplan-maps-v1.0"
+  data_path = "/cailiu2/Diffusion-Planner/data"
+  map_path = "/cailiu2/Diffusion-Planner/data/maps"
+  save_path = "/cailiu2/Diffusion-Planner/data/processed"
+  total_scenarios = 30000
+  scenario_mapping = ScenarioMapping(scenario_map=get_scenario_map(), subsample_ratio_override=0.5)
+  builder = NuPlanScenarioBuilder(data_path, map_path, None, None, map_version, scenario_mapping = scenario_mapping)
+  worker = SingleMachineParallelExecutor(use_process_pool=True)
+  scenario_filter = ScenarioFilter(*get_filter_parameters(num_scenarios_per_type=30000,
+                                                            limit_total_scenarios=total_scenarios))
+  scenarios = builder.get_scenarios(scenario_filter, worker)
+  del worker, builder, scenario_filter
+  processor = DataProcessor(scenarios)
+  processor.work(save_path, debug=False)
+
+
--- diffusion_planner/data_process/utils.py
+++ diffusion_planner/data_process/utils.py
@@ -284,3 +284,123 @@ def convert_to_model_inputs(data, device):
             tensor_data[k] = torch.tensor(v, dtype=torch.float32).unsqueeze(0).to(device)
 
     return tensor_data
+
+# =====================
+# 4. scenario map filter
+# =====================
+def get_scenario_map():
+    scenario_map = {
+        'accelerating_at_crosswalk': [15.0, -3.0],
+        'accelerating_at_stop_sign': [15.0, -3.0],
+        'accelerating_at_stop_sign_no_crosswalk': [15.0, -3.0],
+        'accelerating_at_traffic_light': [15.0, -3.0],
+        'accelerating_at_traffic_light_with_lead': [15.0, -3.0],
+        'accelerating_at_traffic_light_without_lead': [15.0, -3.0],
+        'behind_bike': [15.0, -3.0],
+        'behind_long_vehicle': [15.0, -3.0],
+        'behind_pedestrian_on_driveable': [15.0, -3.0],
+        'behind_pedestrian_on_pickup_dropoff': [15.0, -3.0],
+        'changing_lane': [15.0, -3.0],
+        'changing_lane_to_left': [15.0, -3.0],
+        'changing_lane_to_right': [15.0, -3.0],
+        'changing_lane_with_lead': [15.0, -3.0],
+        'changing_lane_with_trail': [15.0, -3.0],
+        'crossed_by_bike': [15.0, -3.0],
+        'crossed_by_vehicle': [15.0, -3.0],
+        'following_lane_with_lead': [15.0, -3.0],
+        'following_lane_with_slow_lead': [15.0, -3.0],
+        'following_lane_without_lead': [15.0, -3.0],
+        'high_lateral_acceleration': [15.0, -3.0],
+        'high_magnitude_jerk': [15.0, -3.0],
+        'high_magnitude_speed': [15.0, -3.0],
+        'low_magnitude_speed': [15.0, -3.0],
+        'medium_magnitude_speed': [15.0, -3.0],
+        'near_barrier_on_driveable': [15.0, -3.0],
+        'near_construction_zone_sign': [15.0, -3.0],
+        'near_high_speed_vehicle': [15.0, -3.0],
+        'near_long_vehicle': [15.0, -3.0],
+        'near_multiple_bikes': [15.0, -3.0],
+        'near_multiple_pedestrians': [15.0, -3.0],
+        'near_multiple_vehicles': [15.0, -3.0],
+        'near_pedestrian_at_pickup_dropoff': [15.0, -3.0],
+        'near_pedestrian_on_crosswalk': [15.0, -3.0],
+        'near_pedestrian_on_crosswalk_with_ego': [15.0, -3.0],
+        'near_trafficcone_on_driveable': [15.0, -3.0],
+        'on_all_way_stop_intersection': [15.0, -3.0],
+        'on_carpark': [15.0, -3.0],
+        'on_intersection': [15.0, -3.0],
+        'on_pickup_dropoff': [15.0, -3.0],
+        'on_stopline_crosswalk': [15.0, -3.0],
+        'on_stopline_stop_sign': [15.0, -3.0],
+        'on_stopline_traffic_light': [15.0, -3.0],
+        'on_traffic_light_intersection': [15.0, -3.0],
+        'starting_high_speed_turn': [15.0, -3.0],
+        'starting_left_turn': [15.0, -3.0],
+        'starting_low_speed_turn': [15.0, -3.0],
+        'starting_protected_cross_turn': [15.0, -3.0],
+        'starting_protected_noncross_turn': [15.0, -3.0],
+        'starting_right_turn': [15.0, -3.0],
+        'starting_straight_stop_sign_intersection_traversal': [15.0, -3.0],
+        'starting_straight_traffic_light_intersection_traversal': [15.0, -3.0],
+        'starting_u_turn': [15.0, -3.0],
+        'starting_unprotected_cross_turn': [15.0, -3.0],
+        'starting_unprotected_noncross_turn': [15.0, -3.0],
+        'stationary': [15.0, -3.0],
+        'stationary_at_crosswalk': [15.0, -3.0],
+        'stationary_at_traffic_light_with_lead': [15.0, -3.0],
+        'stationary_at_traffic_light_without_lead': [15.0, -3.0],
+        'stationary_in_traffic': [15.0, -3.0],
+        'stopping_at_crosswalk': [15.0, -3.0],
+        'stopping_at_stop_sign_no_crosswalk': [15.0, -3.0],
+        'stopping_at_stop_sign_with_lead': [15.0, -3.0],
+        'stopping_at_stop_sign_without_lead': [15.0, -3.0],
+        'stopping_at_traffic_light_with_lead': [15.0, -3.0],
+        'stopping_at_traffic_light_without_lead': [15.0, -3.0],
+        'stopping_with_lead': [15.0, -3.0],
+        'traversing_crosswalk': [15.0, -3.0],
+        'traversing_intersection': [15.0, -3.0],
+        'traversing_narrow_lane': [15.0, -3.0],
+        'traversing_pickup_dropoff': [15.0, -3.0],
+        'traversing_traffic_light_intersection': [15.0, -3.0],
+        'waiting_for_pedestrian_to_cross': [15.0, -3.0]
+    }
+
+    return scenario_map
+
+def get_filter_parameters(num_scenarios_per_type=None, limit_total_scenarios=None):
+    scenario_types = [
+        'starting_left_turn',
+        'starting_right_turn',
+        'starting_straight_traffic_light_intersection_traversal',
+        'stopping_with_lead',
+        'high_lateral_acceleration',
+        'high_magnitude_speed',
+        'low_magnitude_speed',
+        'traversing_pickup_dropoff',
+        'waiting_for_pedestrian_to_cross',
+        'behind_long_vehicle',
+        'stationary_in_traffic',
+        'near_multiple_vehicles',
+        'changing_lane',
+        'following_lane_with_lead',
+    ]
+
+    scenario_tokens = None              # List of scenario tokens to include
+    log_names = None                     # Filter scenarios by log names
+    map_names = None                     # Filter scenarios by map names
+
+    num_scenarios_per_type = num_scenarios_per_type    # Number of scenarios per type
+    limit_total_scenarios = limit_total_scenarios       # Limit total scenarios (float = fraction, int = num) - this filter can be applied on top of num_scenarios_per_type
+    timestamp_threshold_s = None          # Filter scenarios to ensure scenarios have more than `timestamp_threshold_s` seconds between their initial lidar timestamps
+    ego_displacement_minimum_m = None    # Whether to remove scenarios where the ego moves less than a certain amount
+
+    expand_scenarios = False           # Whether to expand multi-sample scenarios to multiple single-sample scenarios
+    remove_invalid_goals = True         # Whether to remove scenarios where the mission goal is invalid
+    shuffle = False                      # Whether to shuffle the scenarios
+
+    ego_start_speed_threshold = None     # Limit to scenarios where the ego reaches a certain speed from below
+    ego_stop_speed_threshold = None      # Limit to scenarios where the ego reaches a certain speed from above
+    speed_noise_tolerance = None         # Value at or below which a speed change between two timepoints should be ignored as noise.
+
+    return scenario_types, scenario_tokens, log_names, map_names, num_scenarios_per_type, limit_total_scenarios, timestamp_threshold_s, ego_displacement_minimum_m, \
+           expand_scenarios, remove_invalid_goals, shuffle, ego_start_speed_threshold, ego_stop_speed_threshold, speed_noise_tolerance
